19f798fc387ba98276d66cb28f0087bf06045b1c
AnYoungjin <yjanmin@naver.com>
2022-03-17 20:57:03 +0900

    add : PG L2 큰 수 만들기

---
+++ programmers/L2_큰수만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        char[] answer = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = stack.get(i);
+        }
+
+        return new String(answer);
+    }
+}
